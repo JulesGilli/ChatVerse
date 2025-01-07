7dedec8164030b7f39b43be57b06a584dc12f290
Enzo-Alfred <enzo.alfred@epitech.eu>
2025-01-07 15:06:53 +0100

    [ADD] backend

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
-import express from "express";
-import cors from "cors";
-import records from "./routes/record.js";
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
  
 const PORT = process.env.PORT || 5050;
 const app = express();
  
 app.use(cors());
-app.use(express.json());
-app.use("/record", records);
- 
+app.use(bodyParser.json());
+
+const mongoUri = process.env.ATLAS_URI;
+
+mongoose.connect(mongoUri, {})
+.then(()=>console.log('mongoDb connect'))
+.catch((err)=>console.error('mongoDb no connect :', err))
+
 // start the Express server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
