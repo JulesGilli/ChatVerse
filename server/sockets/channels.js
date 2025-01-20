f9b8c21c05b1f9354395f3400e1e1152693f82f9
Gabriel Basso <sendtomadgab@gmail.com>
2025-01-20 10:03:26 +0100

    [ADD] back sockets channel logic

--- server/sockets/channels.js
+++ server/sockets/channels.js
@@ -10,6 +10,44 @@ const channelManager = (socket, io) => {
       console.error('Erreur lors de la création du channel :', err);
     }
   });
-};
 
+  socket.on('deleteChannel', async (data) => {
+    try {
+      const deletedChannel = await Channel.findByIdAndDelete(data.id);
+      console.log("delete channels")
+    } catch (err) {
+      console.error('Erreur suppression du channel :', err);
+    }
+  });
+
+  socket.on('joinChannel', (data) => {
+    try {
+      socket.join(data.channelId);
+      io.to(data.channelId).emit('userJoined', { userId: socket.id, channelId: data.channelId });
+    } catch (err) {
+      console.error('Erreur join channel :', err);
+    }
+  });
+
+
+  socket.on('leaveChannel', (data) => {
+    try {
+      socket.leave(data.channelId);
+      io.to(data.channelId).emit('userLeft', { userId: socket.id, channelId: data.channelId });
+    } catch (err) {
+      console.error('erreur quit channel :', err);
+    }
+  });
+
+  socket.on('listChannels', async () => {
+    try {
+      const channels = await Channel.find({});
+      socket.emit('channelList', channels);
+    } catch (err) {
+      console.error('Erreur lists channels :', err);
+    }
+  });
+
+}
 module.exports = channelManager;
+
