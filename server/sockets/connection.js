134c3df5028cddd1b12fc992b332006076ef6b63
JulesGilli <jules.gilli@epitech.eu>
2025-01-27 13:11:45 +0100

    Revert "[EDIT] - change username"
    This reverts commit 85fbb1dc0088f5fce240c436603810d5e8f1d287.

--- server/sockets/connection.js
+++ server/sockets/connection.js
@@ -6,30 +6,29 @@ let connectedUsers = [];
 const connectionManager = async (socket, io) => {
   let userName = `user${socket.id}`;
   console.log(`${userName} s'est connecté`);
-  connectedUsers.push({ name: userName });
- 
+  connectedUsers.push({ id: socket.id, name: userName });
+
+  io.emit('updateUsers', connectedUsers);
+
   const messageHistory = await Message.find({});
   socket.emit('messageHistory', messageHistory);
- 
+
   socket.on('changeNickname', (data) => {
     const newNickname = data.name;
-    const user = connectedUsers.find((user) => user.name === userName);
+    const user = connectedUsers.find((user) => user.id === socket.id);
     if (user) {
-      let lastName = user.name;
       user.name = newNickname;
-      userName = newNickname;
       io.emit('updateUsers', connectedUsers);
-      socket.emit('updateUsername', userName, lastName);
     }
   });
 
   socket.on('disconnect', () => {
     console.log(`${userName} s'est déconnecté`);
-    connectedUsers = connectedUsers.filter((user) => user.name !== userName);
+    connectedUsers = connectedUsers.filter((user) => user.id !== socket.id);
     io.emit('updateUsers', connectedUsers);
   });
 
   messageManager(socket, io, connectedUsers);
 };
- 
+
 module.exports = connectionManager;
