0d8ddb5c194c714eb9a1910345e016eeada90727
Gabriel Basso <sendtomadgab@gmail.com>
2025-02-02 23:28:54 +0100

    Test

--- server/socket.test.js
+++ server/socket.test.js
@@ -1,57 +1,125 @@
+require('dotenv').config();
 const io = require('socket.io');
 const http = require('http');
 const client = require('socket.io-client');
+const mongoose = require('mongoose');
+const Channel = require('./models/Channel');
+const Message = require('./models/Message');
+const channelManager = require('./sockets/channels');
+const messageManager = require('./sockets/messages');
+const connectionManager = require('./sockets/connection');
 
-describe('Test des sockets', () => {
+const DB_URI = process.env.ATLAS_URI;
+
+describe('Test des sockets - Channel, Message, et Connection Manager', () => {
   let server, ioServer;
-  
-  beforeAll((done) => {
+  const connectedUsers = [];
+
+  // Increase the global timeout for hooks and tests
+  jest.setTimeout(30000);
+
+  beforeAll(async () => {
+    await mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connecté à MongoDB pour les tests');
+
     const httpServer = http.createServer();
     ioServer = io(httpServer);
-    
     ioServer.on('connection', (socket) => {
-      socket.on('message', (data, callback) => { 
-        callback(data);
-      });
+      channelManager(socket, ioServer, connectedUsers);
+      messageManager(socket, ioServer, connectedUsers);
+      connectionManager(socket, ioServer, connectedUsers);
     });
-    
-    server = httpServer.listen(3000, done);
+    // Store the server instance to close later
+    server = await new Promise((resolve) => httpServer.listen(3000, resolve));
   });
 
-  afterAll((done) => {
-    ioServer.close(); 
-    server.close(done);
+  afterAll(async () => {
+    ioServer.close();
+    await new Promise((resolve) => server.close(resolve));
+    await mongoose.connection.close();
   });
 
-  afterEach((done) => {
-    ioServer.sockets.sockets.forEach(socket => socket.disconnect(true));
-    done();
+  afterEach(async () => {
+    // ATTENTION: These operations clear your collections from your production DB!
+    await Message.deleteMany({});
+    await Channel.deleteMany({});
+    ioServer.sockets.sockets.forEach((socket) => socket.disconnect(true));
   });
 
-  test('Doit renvoyer le même message', (done) => {
+  test('Doit envoyer et recevoir un message dans un canal et sauvegarder en DB', (done) => {
     const testClient = client('http://localhost:3000', {
       transports: ['websocket'],
-      forceNew: true
+      forceNew: true,
     });
-
     testClient.on('connect', () => {
-      testClient.emit('message', 'test', (response) => {
-        expect(response).toBe('test');
-        testClient.disconnect();
-        done();
+      testClient.emit('sendMessage', {
+        content: 'Hello, World!',
+        channel: 'general',
+      });
+      testClient.on('newMessage', (message) => {
+        try {
+          expect(message.content).toBe('Hello, World!');
+          expect(message.channel).toBe('general');
+          // Wait briefly to allow the DB insertion to complete
+          setTimeout(async () => {
+            try {
+              const savedMessage = await Message.findOne({ content: 'Hello, World!' });
+              expect(savedMessage).toBeTruthy();
+              expect(savedMessage.content).toBe('Hello, World!');
+              testClient.disconnect();
+              done();
+            } catch (dbErr) {
+              testClient.disconnect();
+              done(dbErr);
+            }
+          }, 500);
+        } catch (err) {
+          testClient.disconnect();
+          done(err);
+        }
       });
     });
-  });
+  }, 10000);
 
-  test('Doit se déconnecter proprement', (done) => {
-    const testClient = client('http://localhost:3000');
-    
-    testClient.on('connect', () => {
-      testClient.disconnect();
-      setTimeout(() => { 
-        expect(testClient.connected).toBe(false);
-        done();
-      }, 100);
+  test("Doit récupérer l'historique des messages d'un canal", (done) => {
+    const testClient = client('http://localhost:3000', {
+      transports: ['websocket'],
+      forceNew: true,
     });
-  });
+    const channelName = 'general';
+    const userName = 'user1';
+    const messageData = {
+      content: 'Test message history',
+      channel: channelName,
+    };
+    new Message({
+      userId: 'user1',
+      userName,
+      content: messageData.content,
+      channel: messageData.channel,
+    })
+      .save()
+      .then(() => {
+        testClient.on('connect', () => {
+          testClient.emit('getMessageHistory', { channel: channelName });
+          testClient.on('messageHistory', (messages) => {
+            try {
+              expect(messages.length).toBeGreaterThan(0);
+              expect(messages[0].content).toBe(messageData.content);
+              testClient.disconnect();
+              done();
+            } catch (error) {
+              testClient.disconnect();
+              done(error);
+            }
+          });
+        });
+      })
+      .catch((err) => done(err));
+  }, 10000);
+
+  
 });
